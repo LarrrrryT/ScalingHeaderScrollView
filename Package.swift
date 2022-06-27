47546ad86e55ee93459c6375b6a7643f75bae53f
Larry <larrytran555@gmail.com>
2022-06-26 21:54:27 -0700

    ios 14 support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -25,6 +25,5 @@ let package = Package(
             name: "ScalingHeaderScrollView",
             dependencies: ["Introspect"],
             path: "Source"),
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
